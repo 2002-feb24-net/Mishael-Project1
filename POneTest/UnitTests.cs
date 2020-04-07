8e1420aaebea9f276af4f92f3130daccb49777eb
MishaelHarrison <mishael.harrison@gmail.com>
2020-04-06 19:14:41 -0500

    hopefully final commit

--- POneTest/UnitTests.cs
+++ POneTest/UnitTests.cs
@@ -1,5 +1,6 @@
 using NUnit.Framework;
 using POne.Lib;
+using System.Data.SQLite;
 
 namespace POneTest
 {
@@ -17,6 +18,25 @@ namespace POneTest
             Assert.AreEqual(true, true);
         }
 
+        [Test]
+        public void Test()
+        {
+            
+        }
 
+        static SQLiteConnection CreateConnection()
+        {
+
+            SQLiteConnection sqlite_conn;
+            // Create a new database connection:
+            sqlite_conn = new SQLiteConnection("Data Source= database.db; Version = 3; New = True; Compress = True; ");
+            // Open the connection:
+            try
+            {
+                sqlite_conn.Open();
+            }
+            catch (System.Exception ex) { }
+            return sqlite_conn;
+        }
     }
 }
